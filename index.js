f3c2c3619d0f9b6bd0c06a158e77e39cb78ffdf4
Odera5 <anikingsleyodera5@gmail.com>
2024-09-08 20:13:09 +0100

    my commit

--- index.js
+++ index.js
@@ -11,95 +11,150 @@ function getComputerChoice(x = Math.round(Math.random() * 2)) {
         console.log("No match found!");
     }
 }
-getComputerChoice();
+
 
 
 
 function getHumanChoice() {
-    let x = window.prompt("choose a word between rock, paper and scissors");
-    x = x.trim().toLowerCase();
-    if (x != "rock" && x != "paper" && x != "scissors") {
-        window.alert("Invalid entry, try again!");
-        getHumanChoice();
-    } else {
-        return x;
+    /* let x = window.prompt("choose a word between rock, paper and scissors");
+     x = x.trim().toLowerCase();
+     if (x != "rock" && x != "paper" && x != "scissors") {
+         window.alert("Invalid entry, try again!");
+         getHumanChoice();
+     } else {
+         return x;
+     }*/
+
+    if (rockButton) {
+        return "rock";
+    }
+    else if (paperButton) {
+        return "paper"
+    }
+    else if (scissorsButton) {
+        return "scissors"
+    }
+    else {
+        console.log("invalid key")
     }
 
-} getHumanChoice();
+}
 
 let humanScore = 0;
 let computerScore = 0;
 
+
+
 function playRound(humanChoice, computerChoice) {
 
     if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("You lose!, paper beats rock");
+
+        para.innerHTML = "You lose!, paper beats rock";
         computerScore += 1;
     }
 
     else if (humanChoice == "rock" && computerChoice == "scissors") {
-        console.log("You win!, rock beats scissors");
+        para.innerHTML = "You win!, rock beats scissors";
         humanScore += 1;
     }
 
     else if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log("You win!, paper beats rock");
+
+        para.innerHTML = "You win!, paper beats rock";
         humanScore += 1;
     }
 
     else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose!, scissors beats paper");
+        para.innerHTML = "You lose!, scissors beats paper";
         computerScore += 1;
     }
 
     else if (humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win!, scissors beats paper");
+        para.innerHTML = "You win!, scissors beats paper";
         humanScore += 1;
     }
 
     else if (humanChoice == "scissors" && computerChoice == "rock") {
-        console.log("You lose!, rock beats scissors");
+        para.innerHTML = "You lose!, rock beats scissors";
         computerScore += 1;
     }
     else {
-        console.log("Draw game!, play again")
+        para.innerHTML = "Draw game!, play again";
     }
 
-    console.log(`You score: ${humanScore}`);
-    console.log(`Computer score: ${computerScore}`);
+    //console.log(`You score: ${humanScore}`);
+    //console.log(`Computer score: ${computerScore}`);
 
+
+    //parap.innerHTML = `You score: ${humanScore}`;
+
+    // parap.innerHTML = `Computer score: ${computerScore}`;
+
+
+    if (humanScore) {
+        parap.innerHTML = `You score: ${humanScore}, Computer score: ${computerScore}`;
+
+    } else {
+        parap.innerHTML = `Computer score: ${computerScore}, You score: ${humanScore}`;
+
+    }
 
 
 }
 
 function playGame() {
-    for (let i = 1; i < 4; i++) {
-        console.log("Round" + i);
-        const humanSelection = getHumanChoice();
-        const computerSelectio = getComputerChoice();
-        playRound(humanSelection, computerSelectio);
+
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+
+
+    if (humanScore == 5) {
+        //parapp.innerHTM = "Final result: You win!";
+        parapp.textContent = "Gameover: You win!   Start Again";
+        //console.log("Final result: You win!");
+    }
+    else if (computerScore == 5) {
+        parapp.textContent = "GameOver: Computer Wins!   Start Again";
+        // parapp.innerHTM = "Final result: You lose!";
+        //console.log("Final result: Computer Wins!");
+    }
+    else if (humanScore && computerScore == 5) {
+        // parapp.innerHTM = "Tie game!";
+        //console.log("Tie game!");
     }
 
-    if (humanScore > computerScore) {
-        console.log("Final result: You win!");
-    }
-    else if (computerScore > humanScore) {
-        console.log("Final result: You lose!");
-    }
-    else {
-        console.log("Tie game!");
-    }
+
 
 }
-playGame();
+
+const rockButton = document.querySelector("#rck");
+const paperButton = document.querySelector("#pap");
+const scissorsButton = document.querySelector("#sci");
+const stopButton = document.querySelector("#stop")
+const para = document.querySelector("#para");
+const parap = document.querySelector("#parap");
+const parapp = document.querySelector("#parapp");
 
 
+rockButton.addEventListener('click', playGame
+)
+
+paperButton.addEventListener('click', playGame
+)
+
+scissorsButton.addEventListener('click', playGame
+)
 
 
+function restartButton() {
 
+    para.textContent = "Choose a Game";
+    parap.textContent = "";
+    parapp.textContent = "";
+}
 
-
-
+stopButton.addEventListener("click", restartButton)
 
 
 
---
+++ test.js
@@ -0,0 +1,34 @@
+/*const container = document.querySelector("#container");
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "this is a glorious text-content!";
+container.appendChild(content);
+
+const paragraph = document.createElement("p");
+paragraph.classList.add("paragraph");
+paragraph.textContent = "Hey i'm red";
+container.appendChild(paragraph);
+
+const headThree = document.createElement("headThree");
+headThree.classList.add("headThree");
+headThree.textContent = "i'm a blue h3!";
+container.appendChild(headThree);
+
+const division = document.createElement("div");
+division.classList.add("division");
+division.style.border = "2px solid black";
+division.style.backgroundColor = "pink";
+container.appendChild(division);
+const headOne = document.createElement("headOne");
+headOne.classList.add("headOne");
+headOne.textContent = "i am in a div";
+division.appendChild(headOne);
+const para = document.createElement("p");
+para.classList.add("para");
+para.textContent = " ME TOO!";
+division.appendChild(para);
+
+const btn = document.querySelector("#btn");
+btn.onclick = () => alert("Hello, World!");*/
+
+
